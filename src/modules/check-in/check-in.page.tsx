8e45bb5d80bf3c0cb87f8682b5a982f9510fe2fb
Hanif Carroll <hanifcarroll@gmail.com>
2024-08-23 16:20:05 -0300

    Use data grid instead of table in check in page

--- src/modules/check-in/check-in.page.tsx
+++ src/modules/check-in/check-in.page.tsx
@@ -10,20 +10,14 @@ import {
   Typography,
   useMediaQuery,
   useTheme,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
   Paper,
-  TablePagination,
 } from '@mui/material';
 import { useCheckInPage } from './use-check-in-page.ts';
 import { LoadingAnimation } from '@/components';
 import { CheckIn } from '@/modules/check-in';
 import { Member } from '@/modules/member';
 import { format, parseISO } from 'date-fns';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
 const MemberSelect: React.FC<{
   selectedMemberId: string;
@@ -49,68 +43,66 @@ const MemberSelect: React.FC<{
   </FormControl>
 );
 
-const CheckInTable: React.FC<{ checkIns: CheckIn[]; isMobile: boolean }> = ({
+function CheckInTable({
   checkIns,
   isMobile,
-}) => {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(20);
+}: {
+  checkIns: CheckIn[];
+  isMobile: boolean;
+}) {
+  const columns: GridColDef[] = [
+    {
+      field: 'date',
+      headerName: 'Date',
+      width: 110,
+      valueGetter: (_, row) => format(parseISO(row.dateTime), 'yyyy-MM-dd'),
+    },
+    {
+      field: 'time',
+      headerName: 'Time',
+      width: 110,
+      valueGetter: (_, row) => format(parseISO(row.dateTime ?? ''), 'HH:mm'),
+    },
+    {
+      field: 'firstName',
+      headerName: 'First Name',
+      width: 130,
+      valueGetter: (_, row) => row.member.firstName,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last Name',
+      width: 130,
+      valueGetter: (_, row) => row.member.lastName,
+    },
+  ];
 
-  const handleChangePage = (_: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+  if (!isMobile) {
+    columns.push({
+      field: 'email',
+      headerName: 'Email',
+      width: 200,
+      valueGetter: (_, row) => row.member.email,
+    });
+  }
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 'calc(100vh - 300px)' }}>
-        <Table stickyHeader aria-label="check-in table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Date</TableCell>
-              <TableCell>Time</TableCell>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              {!isMobile && <TableCell>Email</TableCell>}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {checkIns
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((checkIn) => (
-                <TableRow key={checkIn.id} hover>
-                  <TableCell>
-                    {format(parseISO(checkIn.dateTime), 'yyyy-MM-dd')}
-                  </TableCell>
-                  <TableCell>
-                    {format(parseISO(checkIn.dateTime), 'HH:mm')}
-                  </TableCell>
-                  <TableCell>{checkIn.member.firstName}</TableCell>
-                  <TableCell>{checkIn.member.lastName}</TableCell>
-                  {!isMobile && <TableCell>{checkIn.member.email}</TableCell>}
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 20, 50]}
-        component="div"
-        count={checkIns.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+    <Paper sx={{ width: '100%', height: 'calc(100vh - 300px)' }}>
+      <DataGrid
+        rows={checkIns}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 20 },
+          },
+        }}
+        pageSizeOptions={[10, 20, 50]}
+        checkboxSelection={false}
+        disableRowSelectionOnClick
       />
     </Paper>
   );
-};
+}
 
 export function CheckInPage() {
   const theme = useTheme();
